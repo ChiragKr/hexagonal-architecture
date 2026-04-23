81fb654d98002a4539b81be9e49ac297ed860ee2
Chirag Krishnaswamy <chirag.krishnaswamy@gmail.com>
2026-04-23 23:16:48 +0530

    Add QueueNotificationPort Bean to application context
    NOTE: KafkaQueueAdapter used as QueueNotificationPort implementation

--- bootstrap/src/main/java/com/pluralkraft/notification/configuration/AdapterConfig.java
+++ bootstrap/src/main/java/com/pluralkraft/notification/configuration/AdapterConfig.java
@@ -2,9 +2,13 @@ package com.pluralkraft.notification.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import com.pluralkraft.notification.adapters.inbound.kafka.KafkaNotificationConsumer;
+import com.pluralkraft.notification.adapters.outbound.kafka.KafkaQueueAdapter;
+import com.pluralkraft.notification.domain.model.Notification;
 import com.pluralkraft.notification.ports.in.ProcessNotificationUseCase;
+import com.pluralkraft.notification.ports.out.QueueNotificationPort;
 
 /**
  * Configuration class for notification adapters.
@@ -26,5 +30,20 @@ public class AdapterConfig {
         ProcessNotificationUseCase processNotificationUseCase
     ) {
         return new KafkaNotificationConsumer(processNotificationUseCase);
+    }
+
+    /**
+     * Creates and configures the Kafka-based queue adapter bean.
+     * This bean provides an implementation of QueueNotificationPort that sends
+     * notifications to a Kafka topic for asynchronous processing.
+     *
+     * @param kafkaTemplate the Kafka template to use for sending notifications
+     * @return a configured KafkaQueueAdapter instance
+     */
+    @Bean
+    QueueNotificationPort kafkaQueueAdapter(
+        KafkaTemplate<String, Notification> kafkaTemplate
+    ) {
+        return new KafkaQueueAdapter(kafkaTemplate);
     }
 }
